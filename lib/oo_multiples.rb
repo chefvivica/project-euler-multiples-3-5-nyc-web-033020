747138b97ebf0c7538893a8afb7331a6ff25113a
Vivica Cong Ma <vivicacongma@gmail.com>
2020-10-04 13:55:54 +0000

    Automatically backed up by Learn

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -3,12 +3,12 @@ class Multiples
   attr_reader :limit, :multiples
 
   def initialize(limit)
-    @limit = limit - 1
+    @limit = limit 
     @multiples = collect_multiples
   end
 
   def collect_multiples
-    (1..limit).to_a.reject {|num| num unless num % 3 == 0 || num % 5 == 0 }
+    (1...limit).to_a.reject {|num| num unless num % 3 == 0 || num % 5 == 0 }
   end
 
   def sum_multiples
